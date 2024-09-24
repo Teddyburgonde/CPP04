dff871e5adb73be673abdd4001d8e18923f641fb
Teddyburgonde <bandama.teddy@gmail.com>
2024-09-24 18:55:38 +0200

    added contructor , destructor in AMateria.hpp

--- ex03/AMateria.hpp
+++ ex03/AMateria.hpp
@@ -3,31 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   AMateria.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tebandam <tebandam@student.42.fr>          +#+  +:+       +#+        */
+/*   By: teddybandama <teddybandama@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 11:32:41 by tebandam          #+#    #+#             */
-/*   Updated: 2024/09/24 15:21:27 by tebandam         ###   ########.fr       */
+/*   Updated: 2024/09/24 18:54:21 by teddybandam      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef AMateria
-#define AMateria
+# ifndef AMATERIA_HPP
+#define AMaTERIA_HPP
 
 #include <iostream>
+#include "ICharacter.hpp"
+class ICharacter; // permet de dire que la classe ICharacter existe
+// La precision permet d'eviter les dependances cycliques
 
 class AMateria
 {
-    protected:
-        std::string _type;
     public:
-        AMateria();
-        ~AMateria();
         AMateria(std::string const & type);
-        // ... 
+        AMateria(const AMateria &src); // constructeur par copie
+        AMateria &operator=(const AMateria &src); // operateur d'affectation
+        virtual ~AMateria(); // destructeur
+    public:
         std::string const & getType() const; //Returns the materia type
-
+        // quand un objet qui herite de cette classe est détruit,
+        // le destructeur est appelé
 		virtual AMateria* clone() const = 0;
 		virtual void use(ICharacter& target);
+    protected:
+        std::string _type;
+        AMateria(); // constructeur par defaut
 };
 
 #endif
